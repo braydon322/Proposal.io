3ee95332fca9986b8689ee7d92c46b60fad73f4b
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-12-13 01:11:47 -0800

    Created Nested Forms. Updated Controller with Strong Params. Updated Form

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,5 +1,25 @@
 class ProposalsController < ApplicationController
   def new
     @proposal = Proposal.new
+    @proposal.milestones.build
+    @proposal.fees.build
+  end
+
+  def create
+    @proposal = Proposal.new(proposal_params)
+    if @proposal.save
+      redirect_to @proposal
+    else
+      render :new
+    end
+  end
+
+  def show
+    @proposal = Proposal.find(params[:id])
+  end
+
+  private
+  def proposal_params
+    params.require(:proposal).permit(:email, :title, :budget, :why_me, :milestones_attributes => [:content, :due_date], :fees_attributes => [:content, :pricing_breakdown])
   end
 end
--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -2,4 +2,26 @@ class Proposal < ActiveRecord::Base
   belongs_to :user
   has_many :fees
   has_many :milestones
+
+  def milestones_attributes=(milestones_attributes)
+    milestones_attributes.each do |i, milestone_attributes|
+      self.milestones.build(milestone_attributes)
+    end
+    # Create new milestones for this proposal based on the passed in Params
+      # :milestones_attributes => [
+      #   {:content => "Logo Design/Branding"},
+      #   {:pricing_breakdown => "$175"}
+      # ]
+  end
+
+  def fees_attributes=(fees_attributes)
+    fees_attributes.each do |i, fee_attributes|
+      self.fees.build(fee_attributes)
+    end
+    # Create new fees for this proposal based on the passed in Params
+      # :fees_attributes => [
+      #   {:content => "Logo Design/Branding"},
+      #   {:pricing_breakdown => "$175"}
+      # ]
+  end
 end
