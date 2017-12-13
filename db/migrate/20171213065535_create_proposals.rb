673ad66689e8e4ac6fca038ba808ddd81e8430ae
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-12-12 23:07:41 -0800

    Added Models & Created associations

---
+++ db/migrate/20171213065535_create_proposals.rb
@@ -0,0 +1,20 @@
+class CreateProposals < ActiveRecord::Migration
+  def change
+    create_table :proposals do |t|
+      t.integer :user_id
+      t.integer :why_me
+      t.string :milestone_1
+      t.string :milestone_2
+      t.string :milestone_3
+      t.string :signer_name
+      t.string :title
+      t.string :email
+      t.integer :budget
+      t.string :fee_details
+      t.string :terms
+      t.boolean :invoice_paid, :default => false
+      t.boolean :proposal_accepted, :default => false
+      t.timestamps null: false
+    end
+  end
+end
